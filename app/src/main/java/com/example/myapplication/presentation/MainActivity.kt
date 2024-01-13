c3cf6adafc913d5eeaf51e519f89325dc0a4a41f
MoatazNoaman <moataz.noaman12@gmail.com>
2024-01-13 02:33:08 +0200

    edited Circular Progress Indicator

--- app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.myapplication.presentation
 
+import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -22,7 +25,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -49,23 +55,37 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyApplicationTheme {
                 Surface {
-                    val coins = coinListViewModel.getCoinLive.collectAsState(initial = Resource.Loading(data = emptyList()))
+                    val coins =
+                        coinListViewModel.getCoinLive.collectAsState(initial = Resource.Loading(data = emptyList()))
                     when (coins.value) {
                         is Resource.Error -> {
                             Log.d(TAG, "onCreate: error")
                         }
+
                         is Resource.Loading -> {
                             Log.d(TAG, "onCreate: loading")
-                            CircularProgressIndicator(Modifier.size(30.dp), color = MaterialTheme.colorScheme.onPrimary)
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator(
+                                    Modifier.wrapContentSize(),
+                                    color = Color.Blue,
+                                    strokeCap = StrokeCap.Square,
+                                    strokeWidth = 4.dp
+                                )
+                            }
                         }
+
                         is Resource.Success -> {
                             val state = rememberLazyListState()
-                            LazyColumn(state = state, contentPadding = PaddingValues(12.dp , 4.dp)){
-                                coins.let { coins->
-                                    items(coins.value.data?.size!!){
+                            LazyColumn(state = state, contentPadding = PaddingValues(12.dp, 4.dp)) {
+                                coins.let { coins ->
+                                    items(coins.value.data?.size!!) {
                                         CoinItemList(coin = coins.value.data!![it], clickacle = {
 
                                         })
